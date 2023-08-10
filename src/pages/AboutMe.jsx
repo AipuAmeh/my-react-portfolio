0e2899f21b054d4d6b1bdf79b4439136919b6aa8
AipuAmeh <aipuameh.16@gmail.com>
2023-08-09 22:04:05 -0400

    added about-me section

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -8,17 +8,20 @@ const styles = {
     },
     pageTitle: {
         paddingTop: "5em"
+    },
+    aboutMeText: {
+        margin: "3em 10em",
     }
 }
 
 export default function AboutMeCard() {
     return (
-        <div className="about-me-section" style={styles.content}>
+        <div className="about-me-section mt-5" style={styles.content}>
             <h1>About Me</h1>
 
             <img src="./src/assets/portfoliophoto.jpg" className="headshot" alt="headshot photo" style={styles.headshot}></img>
 
-            <p>My name is Aipu Ameh. I am a Full Stack Web Developer with a focus on visionary technology. I </p>
+            <p className="text-start" style={styles.aboutMeText}> My name is Aipu Ameh. I am a Full Stack Web Developer with a focus on visionary technology. As an avid learner, I use my experiences from various roles that I have held to fuel my life, both personally and professionally. When I am not working, I enjoy spending time with friends and family, traveling, and self-care activities.</p>
         </div>
     )
 }
