ee4e5303997aafbf24fac738e42ba93495495e4e
AipuAmeh <aipuameh.16@gmail.com>
2023-08-10 22:31:53 -0400

    added all projects, linked repo and deployed site

---
+++ src/projects.js
@@ -0,0 +1,41 @@
+const projects = [
+    {
+        name: "Bookipedia",
+        imageSrc: './src/assets/bookipedia.png',
+        deployedLink: 'https://obelisk477.github.io/bookipedia/',
+        gitHub: 'https://github.com/obelisk477/bookipedia',
+        altText: 'bookipedia homepage'
+    },
+    {
+        name: 'Hype-Hub',
+        imageSrc: './src/assets/hype-hub.png',
+        deployedLink:'https://achieved-it-a7cfbbbfd1a9.herokuapp.com/',
+        gitHub: 'https://github.com/erikbenedict/hype-hub'
+    },
+    {
+        name: "Weather Dashboard",
+        imageSrc: './src/assets/weatherdash.png',
+        deployedLink: 'https://aipuameh.github.io/my-weather-dashboard/',
+        gitHub: 'https://github.com/AipuAmeh/my-weather-dashboard'
+    },
+    {
+        name: "Fashion Quiz",
+        imageSrc: './src/assets/fashionquiz.png',
+        deployedLink: 'https://aipuameh.github.io/coding-quiz/',
+        gitHub: 'https://github.com/AipuAmeh/coding-quiz'
+    },
+    {
+        name: "JATE",
+        imageSrc: './src/assets/JATE.png',
+        deployedLink: 'https://obelisk477.github.io/bookipedia/',
+        gitHub: 'https://github.com/obelisk477/bookipedia'
+    },
+    {
+        name: "Daily Scheduler",
+        imageSrc: './src/assets/dailyscheduler.png',
+        deployedLink: 'https://aipuameh.github.io/daily-scheduler/',
+        gitHub: 'https://github.com/AipuAmeh/daily-scheduler'
+    }
+];
+
+export default projects;
