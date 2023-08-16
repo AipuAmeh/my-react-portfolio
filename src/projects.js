468edd8ea62dceaa327c57f121f8b747a867ddff
AipuAmeh <aipuameh.16@gmail.com>
2023-08-15 22:16:17 -0400

    changed image path

--- src/projects.js
+++ src/projects.js
@@ -8,35 +8,35 @@ const projects = [
     },
     {
         name: 'Hype-Hub',
-        imageSrc: '/src/assets/hype-hub.png',
+        imageSrc: '/images/hype-hub.png',
         deployedLink:'https://achieved-it-a7cfbbbfd1a9.herokuapp.com/',
         gitHub: 'https://github.com/erikbenedict/hype-hub',
         altText: 'hype-hub homepage'
     },
     {
         name: "Weather Dashboard",
-        imageSrc: '/src/assets/weatherdash.png',
+        imageSrc: '/images/weatherdash.png',
         deployedLink: 'https://aipuameh.github.io/my-weather-dashboard/',
         gitHub: 'https://github.com/AipuAmeh/my-weather-dashboard',
         altText: 'weather dashboard'
     },
     {
         name: "Fashion Quiz",
-        imageSrc: '/src/assets/fashionquiz.png',
+        imageSrc: '/images/fashionquiz.png',
         deployedLink: 'https://aipuameh.github.io/coding-quiz/',
         gitHub: 'https://github.com/AipuAmeh/coding-quiz',
         altText: 'fashion quiz homepage'
     },
     {
         name: "JATE",
-        imageSrc: '/src/assets/JATE.png',
+        imageSrc: '/images/JATE.png',
         deployedLink: 'https://obelisk477.github.io/bookipedia/',
         gitHub: 'https://github.com/obelisk477/bookipedia',
         altText: 'just another text editor'
     },
     {
         name: "Daily Scheduler",
-        imageSrc: '/src/assets/dailyscheduler.png',
+        imageSrc: '/images/dailyscheduler.png',
         deployedLink: 'https://aipuameh.github.io/daily-scheduler/',
         gitHub: 'https://github.com/AipuAmeh/daily-scheduler',
         altText: 'daily scheduler'
