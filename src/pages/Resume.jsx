e71d7a60c2504b3f4422314af940c08fe157b1f8
AipuAmeh <aipuameh.16@gmail.com>
2023-12-05 15:32:39 -0500

    added styling features

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,31 +1,55 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faJs } from "@fortawesome/free-brands-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faFigma } from "@fortawesome/free-brands-svg-icons";
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { faCss3 } from "@fortawesome/free-brands-svg-icons";
+import { faGit } from "@fortawesome/free-brands-svg-icons";
+import { faBootstrap } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+import { faJava } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
 
 const styles = {
-    content: {
-        paddingBottom: "18em"
-    }
+  content: {
+    marginBottom: "23em",
+  },
+  skills: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+  },
+  iconSize: {
+    height: "2em",
+  },
+};
+
+export default function Resume() {
+  return (
+    <div className="resume" style={styles.content}>
+      {/* <h1 className='resume-header mt-5'>Resume</h1> */}
+      <h4 className="mt-5">Some of my skills include:</h4>
+      <div className="skills-list mt-5 mb-5" style={styles.skills}>
+        <FontAwesomeIcon style={styles.iconSize} icon={faFigma} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faJs} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faReact} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faNodeJs} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faDatabase} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faCss3} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faGit} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faBootstrap} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faHtml5} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faJava} />
+        <FontAwesomeIcon style={styles.iconSize} icon={faPython} />
+      </div>
+      <Link
+        className="resume-link pb-5"
+        to="https://drive.google.com/file/d/1wBr1JqEOZ_9gLJ60fs9jPjURFsvT4cyB/view?usp=sharing"
+      >
+        Click Here to View My Full Resume!
+      </Link>
+    </div>
+  );
 }
- 
- export default function Resume() {
-    return (
-        <div className="resume" style={styles.content}>
-            {/* <h1 className='resume-header mt-5'>Resume</h1> */}
-            <h4 className='mt-5'>As a Full Stack Developer with a background in UI/UX Design, I am proficient in the following skills:</h4>
-            <ul className='skills-list mt-3'>
-                <li>Figma/AdobeXD</li>
-                <li>Wireframing</li>
-                <li>Prototyping</li>
-                <li>Responsive Web Design</li>
-                <li>Node.JS</li>
-                <li>JQuery</li>
-                <li>MySQL/MongoDB</li>
-                <li>React</li>
-                <li>CSS Frameworks</li>
-                <li>JavaScript</li>
-            </ul>
-            <Link 
-            className='resume-link'
-            to='https://drive.google.com/file/d/1wBr1JqEOZ_9gLJ60fs9jPjURFsvT4cyB/view?usp=sharing'>Click Here to View My Full Resume!</Link>
-        </div>
-    )
- }
